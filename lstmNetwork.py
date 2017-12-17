68f780c5412e25ec30357760dec86154e1f313ec
mstanojevic118 <mstanojevic118@users.noreply.github.com>
2017-12-16 22:39:12 -0500

    Update lstmNetwork.py

--- lstmNetwork.py
+++ lstmNetwork.py
@@ -8,37 +8,8 @@ import numpy as np
 #changes data to create sequences of length 7 that are input in LSTM network
 #saves reorganized data into files contianing 7 in name
 def dataPreparation(fileName, dataX, dataY, dataCvX, dataCvY, dataTestX):
-    randomWeeks = readFile('randomWeeks.p')
-    k = 1 # week in year
-    l = 0 # current row in dataX
-    trainX = [] # dimensions [?, 7, 120]
-    trainY = [] # dimensions [?, 7, 1]
-    cvX = [] # dimensions [?, 7, 120]
-    cvY = [] # dimensions [?, 7, 1]
-    testX = [] # dimensions [?, 7, 120]
-    testLabel = [] # dimensions [?, 7, 1]
-    for i in range(0, 240):
-        for j in range(0, 7):
-            if (dataX[l][4] != k):
-                k = dataX[l][4]
-                k += 1
-                break
-            elif (j == dataX[l][3]):
-                trainX[i][j] = dataX[l] #TODO change this, because one product can be sold in different locations, we need to sum them all
-                trainY[i][j] = dataY[l]
-                l += 1
-            elif (j < l):
-                trainX[i][j] = 1 #TODO: make this a 120 list that has values for 3, 4, 5,
-                trainX[i][j] = 0
-    # TODO I didn't incorporte all the possibilities (weeks starting from Monday, Tuesday,...)
-    # TODO input all dates starting from 29th December 2012 in trainX (as mean of previous and next date)
-    # TODO input all missing dates in random weeks in cvX (as mean of previous and next date or one of them if there is no other)
-    saveFile('X' + fileName, trainX)
-    saveFile('Y' + fileName, trainY)
-    saveFile('X' + fileName.replace('train', 'test'), testX)
-    saveFile('label' + fileName.replace('train', 'test'), testLabel)
-    saveFile('cvX' + fileName, cvX)
-    saveFile('cvY' + fileName, cvY)
+    # This function is not finished, because I realized I don't have appropriate data for LSTM model
+    # I still uploaded it, because other functions are good and can be used
     return trainX, trainY, cvX, cvY, testX
 
 #train neural network for each item separately; neural network is implemented in tensorflow
